587952f587f242a2cc46ddd65742e25d104733c9
elMofaker <155926314+elMofaker@users.noreply.github.com>
2024-07-19 03:54:42 +0300

    d

--- js.js
+++ js.js
@@ -1,18 +1,41 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY > 20) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
+window.addEventListener("scroll", function () {
+  const element = document.querySelector(".change-color");
+  if (window.scrollY > 50) {
+    element.classList.add("scrolled");
+  } else {
+    element.classList.remove("scrolled");
+  }
 });
 
-  // scroll trans
-ScrollReveal({ 
-    distance: '80px',
-    duration: 1400,
+// scroll trans
+ScrollReveal({
+  distance: "80px",
+  duration: 1400,
 });
 
-ScrollReveal().reveal('.name', { delay: 250, origin: 'left', interval: 200});
-ScrollReveal().reveal('.right', { delay: 250, origin: 'right'});
-ScrollReveal().reveal('.top', { delay: 250, origin: 'top', interval: 100});
+ScrollReveal().reveal(".name", { delay: 250, origin: "left", interval: 200 });
+ScrollReveal().reveal(".right", { delay: 250, origin: "right" });
+ScrollReveal().reveal(".top", { delay: 250, origin: "top", interval: 100 });
+
+
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel",(evt) => {
+
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltay
+  scrollContainer.style.scrollBehavior = "auto";
+
+});
+
+nextBtn.addEventListener("click", ()=>{
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", ()=>{
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 900;
+});
